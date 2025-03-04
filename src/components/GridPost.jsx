6f4798286d647e525bddca833f08dbf1dd95cb4b
Jules HG <jhacq001@ucr.edu>
2025-03-04 15:51:15 -0800

    Profile header & grid posts css

--- src/components/GridPost.jsx
+++ src/components/GridPost.jsx
@@ -7,37 +7,43 @@ const GridPost = () => {
   const [like, setLike] = useState(false);
 
   return (
-    <div id="gridpost">
-      <div id="post-header">
-        <div id="title">
-            <div id="profile-pic"></div>
-            <div id="display-name">IEatDogFood</div>
+    <div class="gridpost">
+      <div class="gridpost-side-decor gridpost-left-decor"></div>
+
+      <div class="gridpost-interior">
+        <div class="gridpost-header">
+          <div class="gridpost-info-holder">
+              <div class="gridpost-profile-pic"></div>
+              <div /*id="display-name"*/>IEatDogFood</div>
+          </div>
+          <div class="gridpost-time-stamp">
+            <span className="date">12/12/2024</span>
+            <span className="time">3:01 PM</span>
+          </div>
         </div>
-        <div id="time-stamp">
-          <span className="date">12/12/2024</span>
-          <span className="time">3:01 PM</span>
+
+        {/* Content */}
+        <p class="gridpost-content">I am a good doggo. Munch munch CRONCH chew chew</p>
+
+        {/* Buttons */}
+        <div class="gridpost-button-holder">
+          <div class="gridpost-like-button" onClick={() => setLike(!like)}>
+            {like ? (
+              <Heart className="heart" fill="#BE4A31" strokeWidth={0} />
+            ) : (
+              <Heart className="heart" strokeWidth={1.3} color="#878787" />
+            )}
+            <p /*id="like-count"*/>1024</p>
+          </div>
+
+          <div class="gridpost-comment-button">
+            <MessageCircle className="comment-button" />
+            <p /*id="comment-count"*/>2000</p>
+          </div>
         </div>
       </div>
 
-      {/* Content */}
-      <p id="content">I am a good doggo.</p>
-
-      {/* Buttons */}
-      <div id="buttons">
-        <div id="like-button" onClick={() => setLike(!like)}>
-          {like ? (
-            <Heart className="heart" fill="#BE4A31" strokeWidth={0} />
-          ) : (
-            <Heart className="heart" strokeWidth={1.3} color="#878787" />
-          )}
-          <p id="like-count">1024</p>
-        </div>
-
-        <div id="comment-button">
-          <MessageCircle className="comment-button" />
-          <p id="comment-count">2000</p>
-        </div>
-      </div>
+      <div class="gridpost-side-decor gridpost-right-decor"></div>
     </div>
   );
 };
--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -3,18 +3,18 @@ import "@/styles/profile_header.css";
 
 const ProfileHeader = () => {
     return (
-        <div id="section">
+        <div class="profile-header">
 
             {/* Temp Profile Pic*/}
-            <div id="profile-pic"></div>
+            <div class="profile-pic"></div>
 
-            <div id="display-bio">
-                <p id="display-name">IEatDogFood</p>
-                <p id="bio">I am dog. I eat dog food and occasionally my owner’s food. My owner loves me very much. I am a good doggo.</p>
+            <div class="display-bio-holder">
+                <p class="display-name">IEatDogFood</p>
+                <p class="header-bio">I am dog. I eat dog food and occasionally my owner’s food. My owner loves me very much. I am a good doggo.</p>
             </div>
             
 
-            <div id="buttons">
+            <div class="profile-button-holder">
                 <Button title="Follow" />
                 <Button title="Message" />
                 <Button title="Report" />
