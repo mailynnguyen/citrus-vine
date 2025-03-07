aa9b5fa7983b6123b68c2625e752e9e375205c64
Jules HG <jhacq001@ucr.edu>
2025-03-06 22:24:41 -0800

    Profile page routing

---
+++ src/app/profile/[slug]/page.js
@@ -0,0 +1,25 @@
+import ProfileHeader from "@/components/ProfileHeader";
+import GridPosts from "@/components/GridPosts";
+
+import GridPost from "@/components/GridPost";
+
+export async function generateStaticParams() {
+    const users = await fetch('http://localhost:3307/Users/FetchAll').then((res) => res.json())
+   
+    return users.map((user) => ({
+      UserID: user.UserID,
+    }))
+}
+
+export default async function Page({ params }) {
+    const {slug} = await params
+
+    return(
+        {slug} && 
+        <div>
+            <ProfileHeader></ProfileHeader>
+            <GridPosts id={slug}></GridPosts>
+        </div>
+
+    )
+}
