ce59d270d0e4ba7363a2b84ff58f0bc89adc2c49
Katswato <ujicofluff@gmail.com>
2025-03-08 23:54:51 -0800

    [Component] Added basic functionality, pends tests, dehydration fixed

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,12 +4,44 @@ import { Heart } from 'lucide-react';
 import '@/styles/signin.css';
 import CitrusBg from "../images/citrus-bg.jpeg";
 
+import { UsersValidateAccount } from "@/app/paths";
+
 function SignIn() {
     const [user, enterUser] = useState("");
     const [password, enterPassword] = useState("");
 
+    const AttemptSignIn = async(enteredUser, enteredPassword) => {
+        try {
+            var submission = {
+                "Username": enteredUser,
+                "Password": enteredPassword
+            }
+            var result = axios.post(UsersValidateAccount, submission)
+            console.log("[AttemptSignIn][Try]: ", result)
+            return result
+        }
+        catch(err) {
+            console.log("Error: ", err)
+        }
+    }
+    const ClickSignIn = (enteredUsername, enteredPassword) => {
+        AttemptSignIn(enteredUsername, enteredPassword)
+            .then((result) => {
+            var valid = result.data[0].Outcome
+            console.log("[AttemptSignIn][.then]: ", valid)
+
+            if (valid) {
+                console.log("Successful sign in!")
+            }
+            else {
+                alert("Invalid username or password.")
+            }
+        })
+    }
+    
+
     return(
-        <body id="bg-holder" style={{backgroundImage: `url(${CitrusBg.src})`}}>
+        <section id="bg-holder" style={{backgroundImage: `url(${CitrusBg.src})`}}>
             <div className="sign-in-text">Citrus Vine</div>
 
             <form className="sign-in-form">
@@ -17,7 +49,7 @@ function SignIn() {
 
                 <input type="password" id="passwordEntry" className="input-text" placeholder="Enter password..." value={password} onChange={(e) => enterPassword(e.target.value)}></input>
                 
-                <input type="submit" value="Sign In" className="sign-in-button"></input>
+                <input type="submit" value="Sign In" className="sign-in-button" onClick={() => ClickSignIn(enteredUsername, enteredPassword)}></input>
             </form>
 
             {/*Need to implement functionality for buttons*/}
@@ -26,7 +58,7 @@ function SignIn() {
 
                 <button className="sign-in-button">Forgot password?</button>
             </div>
-        </body> 
+        </section> 
     )
 }
 
