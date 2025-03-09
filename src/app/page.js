cd34c09b199458f4e3fefdd913c330e8f9759d84
Katswato <ujicofluff@gmail.com>
2025-03-09 15:31:46 -0700

    [HomePage] Hotfix demo of new feed

--- src/app/page.js
+++ src/app/page.js
@@ -5,34 +5,13 @@ import PostModal from "@/components/PostModal";
 import Posts from "@/components/Posts";
 import SearchBar from "@/components/SearchBar";
 import SideBar from "@/components/SideBar";
+import Feed from "@/components/Feed";
 
 export default function Home() {
 
-  const [create, setCreate] = useState(false);
-
-  const handleOpen = (e) => {
-    e.preventDefault();
-    setCreate(true)
-    document.body.style.overflow = "hidden"; // Disable scrolling when modal is open
-    // console.log(create)
-  }
-
-  const handleClose = (e) => {
-    e.preventDefault()
-    setCreate(false)
-    document.body.style.overflow = "auto"; // Disable scrolling when modal is open
-  }
-
-  return (
+  return(
     <div>
-      <div>
-        <SearchBar />
-        <Posts />
-      </div>
-      
-      <SideBar onClick={handleOpen}/>
-
-      {create ? <PostModal onClick={handleClose} setCreate={setCreate} /> : ""}
+      <Feed/>
     </div>
-  );
+  )
 }
