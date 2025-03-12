8716aa070fdc1c9571f7f30087a1d37c5eefc99a
atham011 <atham011@ucr.edu>
2025-03-12 12:22:35 -0700

    Works for all 3 sign in options, logout still needs to be tested

--- src/app/api/auth/googleSignIn.js
+++ src/app/api/auth/googleSignIn.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import axios from "axios";
+import {UsersCreateSession} from "../../paths.js"
 import { findUserByEmail, insertNewUserByEmail } from '../../models/userModels.js';
 
 
@@ -22,10 +24,13 @@ router.post('/googleSignIn', async (req, res) => {
         }
 
         const userExists = await findUserByEmail(email);
-        
+        // find username  by the email, and then begin session
         if (userExists) {
+            const username = userExists.Username
+            const result = await axios.post(UsersCreateSession, {Username: username}, { withCredentials: true });
+            console.log("✅ Sign-In Successful. UserID:", result.data.userID);
             console.log("An account with this email already exists.");
-            
+
             return res.status(200).json({ message: 'Google Sign-in Successful!'});
             // return res.status(409).json({ message: 'Email already exists'});
         }
